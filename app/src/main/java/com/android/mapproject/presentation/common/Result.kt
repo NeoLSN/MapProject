93839f45c66e33ba4afd29f65dbc41ad2833b982
Jason Yang <jasonyang@exosite.com>
2018-10-12 15:43:48 +0800

    Enhance `Result.InProgress` capability

--- app/src/main/java/com/android/mapproject/presentation/common/Result.kt
+++ app/src/main/java/com/android/mapproject/presentation/common/Result.kt
@@ -5,7 +5,11 @@ package com.android.mapproject.presentation.common
  */
 sealed class Result<T>(val inProgress: Boolean) {
 
-    class InProgress<T> : Result<T>(true) {
+    class InProgress<T> constructor(
+            val isIndeterminate: Boolean,
+            val steps: Int,
+            val progress: Int
+    ) : Result<T>(true) {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
@@ -20,7 +24,8 @@ sealed class Result<T>(val inProgress: Boolean) {
     data class Failure<T>(val errorMessage: String?, val e: Throwable) : Result<T>(false)
 
     companion object {
-        fun <T> inProgress(): Result<T> = InProgress()
+        fun <T> inProgress(isIndeterminate: Boolean = true, steps: Int = 0, progress: Int = 0): Result<T>
+                = InProgress(isIndeterminate, steps, progress)
 
         fun <T> success(data: T): Result<T> = Success(data)
 
